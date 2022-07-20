7ba13b6fbecc5066930b0d7e502c289fcdc90f87
philserra <psm13@hotmail.fr>
2022-07-20 15:24:04 +0200

    phil

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,8 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\RestaurantController;
-=======
 use App\Http\Controllers\RestaurateurController;
->>>>>>> d91f777ef65f71f401a129fe0b7d6c8a7f4d5999
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,13 +18,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::resources('/home', MenuController::class);
-<<<<<<< HEAD
 Route::resource('/restaurants', RestaurantController::class);
 // Route::post('/restaurant', [RestaurantController::class , 'store']);
-=======
 
 
 Route::resource('/restaurateurs', RestaurateurController::class);
 
 // Route::post('/restaurateurs', [RestaurateurController::class, 'store'])->name('restaurateurs.store');
->>>>>>> d91f777ef65f71f401a129fe0b7d6c8a7f4d5999
